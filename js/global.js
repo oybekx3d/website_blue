8b0675d2340e457ce26bf2d46e949509caf323a2
Oybek Qodirberdiyev <101323670+oybekx3d@users.noreply.github.com>
2024-06-17 14:20:08 +0300

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -6,22 +6,56 @@ function updatemenu() {
       document.getElementById('menu').style.borderRadius = '0';
     }
   }
-function updateGlobal() {
-  fetch('data/global.json')
-      .then(response => response.json())
-      .then(data => {
-        $("#announcement").html(data.announcement);
-        $("#announcement").attr("href", data.announcementLink);
-        });
+
+function loadMenu() {
   fetch('menu.txt')
       .then(response => response.text())
       .then(data => {
         $('#menu').html(data)
         });
+}
+
+function loadFooter() {
   fetch('footer.txt')
       .then(response => response.text())
       .then(data => {
         $('footer').html(data)
+        });  
+}
+function globaljs() {
+  fetch('data/global.json')
+      .then(response => response.json())
+      .then(data => {
+        $("#announcement").html(data.announcement);
+        $("#announcement").attr("href", data.announcementLink);
+        let letsKnowCont = document.createElement("div");
+        letsKnowCont.id = "unavailableDiscordPopUp";
+        let letsKnowCloseBtn = document.createElement("button");
+        $(letsKnowCloseBtn).text("X");
+        $(letsKnowCloseBtn).attr("onclick","$('#unavailableDiscordPopUp').hide()")
+        $(letsKnowCloseBtn).attr("id", "popUpCloseBtn")
+        let letsknowtext = document.createElement("h3");
+        $(letsknowtext).text(data.letsKnowText);
+        let letsKnowPar = document.createElement("p");
+        $(letsKnowPar).text(data.letsKnowPar)
+        let letsKnowBtnLink = document.createElement("button");
+        $(letsKnowBtnLink).text(data.letsKnowBtnText)
+        $(letsKnowBtnLink).attr("onclick", "window.open('" + data.letsKnowLink+"')")
+        letsKnowCont.appendChild(letsKnowCloseBtn);
+        letsKnowCont.appendChild(letsknowtext);
+        letsKnowCont.append(letsKnowPar);
+        letsKnowCont.appendChild(letsKnowBtnLink);
+        document.body.appendChild(letsKnowCont)
         });
+  
+}
+
+
+
+
+function updateGlobal() {
+  loadMenu();
+  loadFooter();
+  globaljs();
 }
 updateGlobal()
